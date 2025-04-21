0a07bc5709fe3604a6698fedf783e56daf888105
AntonioB0513 <abravovieira@gmail.com>
2025-04-21 18:33:14 -0400

    configuraciones

--- frontend/FRONTEND-main/vite.config.js
+++ frontend/FRONTEND-main/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    // Bind al 0.0.0.0 para que Railway pueda exponerlo
+    host: '0.0.0.0',
+    // Puerto que Railway te da en $PORT
+    port: Number(process.env.PORT) || 4173,
+    strictPort: true,
+    // Lista blanca de hosts que Vite acepta en el header Host
+    allowedHosts: [
+      'practical-gratitude-production.up.railway.app'
+    ]
+  }
 })
